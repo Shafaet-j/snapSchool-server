ba6c90a073f39eb710de305abba1bb8554c788e0
shafaet <lbshaft01@gmail.com>
2023-06-07 18:52:50 +0600

    check user exist or not

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
     // user related api 
     app.post('/users', async(req,res)=>{
         const user = req.body;
+        const filter = {email: user.email}
+        const existingUser = await userCollection.findOne(filter)
+        if(existingUser){
+            return res.send({message: 'user already existed'})
+        }
         const result = await userCollection.insertOne(user)
         res.send(result)
     })
