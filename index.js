d849af5a70c01c5d3fb6c8c16344de4d7cc95886
shafaet <lbshaft01@gmail.com>
2023-06-07 21:40:21 +0600

    user role make admin done

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const app = express()
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// middleware 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fluahev.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,31 +25,54 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const userCollection = client.db('snapschool').collection('user')
+    const userCollection = client.db("snapschool").collection("user");
 
-    // user related api 
+    // user related api
 
-    app.get('/users', async(req,res)=>{
-        const result = await userCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.post('/users', async(req,res)=>{
-        const user = req.body;
-        const filter = {email: user.email}
-        const existingUser = await userCollection.findOne(filter)
-        if(existingUser){
-            return res.send({message: 'user already existed'})
-        }
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-    })
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const existingUser = await userCollection.findOne(filter);
+      if (existingUser) {
+        return res.send({ message: "user already existed" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
-
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -60,13 +80,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("snapschool is running");
+});
 
-
-
-app.get('/', (req,res)=>{
-    res.send('snapschool is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`snapschool is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`snapschool is running on port ${port}`);
+});
