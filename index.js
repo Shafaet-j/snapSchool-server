8d1fad2415357cebe6197fbad3400b795fcc9ae8
shafaet <lbshaft01@gmail.com>
2023-06-08 11:25:43 +0600

    verify jwt

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorization access" });
+  }
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorization access" });
+    }
+    req.decoded = decoded;
+    next()
+  });
+};
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fluahev.mongodb.net/?retryWrites=true&w=majority`;
 
